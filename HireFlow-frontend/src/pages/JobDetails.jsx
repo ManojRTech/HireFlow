ae670d038a8ca4929f46dbf618f4c024c76c9ac8
Manoj R <manoj.r1357@gmail.com>
2026-03-04 11:58:15 +0530

    fix apply navigate.

--- HireFlow-frontend/src/pages/JobDetails.jsx
+++ HireFlow-frontend/src/pages/JobDetails.jsx
@@ -36,7 +36,7 @@ function JobDetails() {
 
           {role === "JOB_SEEKER" && (
             <button
-              onClick={() => Navigate(`/jobs/${id}/apply`)}
+              onClick={() => navigate(`/jobs/${id}/apply`)}
               className="mt-6 bg-green-600 text-white px-4 py-2 rounded"
             >
               Apply Now
